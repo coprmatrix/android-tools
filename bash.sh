45add567f8996279554e71523764814338d99aa2
u <u>
2025-05-05 08:30:18 +0300

    update

---
+++ bash.sh
@@ -0,0 +1,7 @@
+cd "${outdir:-.}"
+git clone https://src.fedoraproject.org/rpms/objectweb-asm.git
+pushd objectweb-asm
+sed -i 's/%bcond_with\s+bootstrap/%bcond_without\sbootstrap/' objectweb-asm.spec
+popd
+mv objectweb-asm/* ./
+
