13d8da0b328180ce0de83bd17af546d813ef6768
u <u>
2025-05-05 08:51:53 +0300

    update

--- .osc.temp/_sources_dir/bash.sh
+++ .osc.temp/_sources_dir/bash.sh
@@ -2,6 +2,7 @@ cd "${outdir:-.}"
 git clone https://src.fedoraproject.org/rpms/objectweb-asm.git
 pushd objectweb-asm
 sed -i 's/%bcond_with\s+bootstrap/%bcond_without\sbootstrap/' objectweb-asm.spec
+bash -x generate-tarball.sh
 popd
 mv objectweb-asm/* ./
 
