48e1cd3cd6a7eaaa73fedb44851a802cd4d9b288
u <u>
2025-05-08 01:48:21 +0300

    update

--- .osc.temp/_sources_dir/bash.sh
+++ .osc.temp/_sources_dir/bash.sh
@@ -1,10 +1,8 @@
 name="android-tools"
 cd "${outdir:-.}"
-git clone https://src.fedoraproject.org/rpms/"$name".git
+git clone https://github.com/OpenMandrivaAssociation/android-tools.git
 pushd "$name"
-sed -i '3i BuildRequires: zlib-ng-compat-devel' "$name".spec
-sed -i '2i Patch9: https://github.com/nmeum/android-tools/blob/master/patches/extras/0003-extras-libjsonpb-Fix-incompatibility-with-protobuf-v.patch' "$name".spec
-bash -x generate-tarball.sh
+sed -i 's~selinux-static-devel~zlib-ng-compat-devel~g;' "$name".spec
 popd
 mv "$name"/* ./
 
