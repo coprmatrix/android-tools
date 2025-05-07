e2331d8663463bcf0a6832b032231f65e247a1b1
u <u>
2025-05-08 00:55:28 +0300

    update

--- bash.sh
+++ bash.sh
@@ -3,6 +3,7 @@ cd "${outdir:-.}"
 git clone https://src.fedoraproject.org/rpms/"$name".git
 pushd "$name"
 sed -i '3i BuildRequires: zlib-ng-compat-devel' "$name".spec
+sed -i '2i Patch9: https://github.com/nmeum/android-tools/blob/master/patches/extras/0003-extras-libjsonpb-Fix-incompatibility-with-protobuf-v.patch' "$name".spec
 bash -x generate-tarball.sh
 popd
 mv "$name"/* ./
